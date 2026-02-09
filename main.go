c392bf74d6187dcabc90701b692605279b6ffb31
VIJAY GANGISHETTI <vijaygangishetti@VIJAYs-MacBook-Air.local>
2026-02-09 22:02:08 +0530

    C: Updated the code in main.go file and added post route successfully

---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+const fileName = "posts.json"
+
+type Post struct {
+	ID        string `json:"id"`
+	Title     string `json:"title"`
+	Content   string `json:"content"`
+	Author    string `json:"author"`
+	CreatedAt string `json:"createdAt"`
+	UpdatedAt string `json:"updatedAt"`
+}
+
+func loadPosts() ([]Post, error) {
+
+	if _, err := os.Stat(fileName); os.IsNotExist(err) {
+		err := os.WriteFile(fileName, []byte("[]"), 0644)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	var posts []Post
+	err = json.Unmarshal(data, &posts)
+
+	return posts, err
+}
+func savePosts(posts []Post) error {
+
+	data, err := json.MarshalIndent(posts, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(fileName, data, 0644)
+}
+
+func generateID() string {
+	return fmt.Sprintf("%d", time.Now().UnixNano())
+}
+
+func getTime() string {
+	return time.Now().Format(time.RFC3339)
+}
+
+func createPost(c *gin.Context) {
+
+	var newPost Post
+
+	if err := c.ShouldBindJSON(&newPost); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
+		return
+	}
+
+	if newPost.Title == "" || newPost.Content == "" || newPost.Author == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Missing fields"})
+		return
+	}
+
+	posts, err := loadPosts()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "File read error"})
+		return
+	}
+
+	now := getTime()
+
+	newPost.ID = generateID()
+	newPost.CreatedAt = now
+	newPost.UpdatedAt = now
+
+	posts = append(posts, newPost)
+
+	if err := savePosts(posts); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "File write error"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, newPost)
+}
+
+func main() {
+
+	gin.SetMode(gin.ReleaseMode)
+
+	router := gin.Default()
+
+	router.SetTrustedProxies(nil)
+
+	// Health check
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Server is running"})
+	})
+
+	// POST route only
+	router.POST("/posts", createPost)
+
+	fmt.Println("Server running at http://localhost:8080")
+
+	router.Run(":8080")
+}
